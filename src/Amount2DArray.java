e9c981c005f8bb2f0633c1e355e589753dcd3244
ragor114 <69634980+ragor114@users.noreply.github.com>
2021-12-23 10:35:07 +0100

    added solution

--- src/Amount2DArray.java
+++ src/Amount2DArray.java
@@ -8,7 +8,27 @@ public class Amount2DArray {
         Wert überführt werden.
      */
     public static int[][] turnAllElementsOf2DArrayToAmound(int[][] values) {
-        return null;
+        // Leere 2D-Arrays werden unverändert zurückgetgeben.
+        if(values == null || values.length == 0 || values[0].length == 0) {
+            System.out.println("Array empty");
+            return values;
+        }
+
+        /*
+            Das 2D-Array wird mit einer doppelten for-Schleife komplett durchlaufen
+            Die obere Schleife durchläuft alle "Reihen"
+            Die untere Schleife durchläuft alle "Spalten"
+         */
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values[0].length; j++) {
+                // Wird ein negativer Wert gefunden, so wird dieser durch Multiplikation mit -1 in einen positiven umgewandelt.
+                if(values[i][j] < 0) {
+                    values[i][j] *=-1;
+                }
+            }
+        }
+
+        return values;
     }
 
     public static void main(String[] args) {
@@ -17,7 +37,7 @@ public class Amount2DArray {
 
         int[][] test = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
 
-        if(Arrays.equals(converted, test)) {
+        if(Arrays.deepEquals(converted, test)) {
             System.out.println("Correct");
         } else {
             System.out.println("Not all numbers turned to Amount.");
